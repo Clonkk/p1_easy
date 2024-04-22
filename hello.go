cc9a16b0d61e9afa2cf69a1a0c0c5549a6c48f78
Clonkk <rf.clonk@linuxmail.org>
2024-04-22 16:36:13 +0200

    add prototype for err handling

--- hello.go
+++ hello.go
@@ -3,12 +3,32 @@ package main
 import (
 	"example/greetings"
 	"fmt"
+	"log"
 
 	"rsc.io/quote"
 )
 
+func safeGreeting(name string) {
+	message, err := greetings.Hello(name)
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned message
+	// to the console.
+	fmt.Println(message)
+}
+
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("[greetings] => ")
+	log.SetFlags(0)
+
 	fmt.Println("Hello, world !")
 	fmt.Println(quote.Go())
-	fmt.Println(greetings.Hello("Test 1"))
+	safeGreeting("Clonkk")
+	safeGreeting("")
 }
